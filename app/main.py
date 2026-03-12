591902ceceb007b0435eb764776106c1d3184ccc
kundan kumar <kundan.karri@cloudium.io>
2026-03-12 19:16:40 +0530

    changes

--- app/main.py
+++ app/main.py
@@ -4,4 +4,4 @@ app = FastAPI(title="My FastAPI Project")
 
 @app.get("/")
 def home():
-    return {"message": "Kundan FastAPI Project Running with Docker and Pipelines"}
+    return {"message": "Kundan FastAPI Project Running with Docker and Pipelines!"}
